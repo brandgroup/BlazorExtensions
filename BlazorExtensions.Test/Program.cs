419d62dc9cdf02735d34058625f1e474b0a56b50
Jan R <jan.roslan@protonmail.com>
2024-08-09 23:05:14 +0200

    Add project files.

---
+++ BlazorExtensions.Test/Program.cs
@@ -0,0 +1,54 @@
+using Brandgroup.BlazorExtensions.Test.Components;
+using Microsoft.AspNetCore.Authentication.Negotiate;
+using DevExpress.Blazor;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+    .AddNegotiate();
+
+builder.Services.AddAuthorization(options => {
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+
+builder.Services.AddCascadingAuthenticationState();
+
+// Add services to the container.
+builder.Services
+    .AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMvc();
+
+
+// Add options to the container.
+
+// Add services to the container.
+// ...
+
+// Add DevExpress services to the container.
+builder.Services.AddDevExpressBlazor(configure =>
+    configure.BootstrapVersion = BootstrapVersion.v5);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
