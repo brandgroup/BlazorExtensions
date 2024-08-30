c3e199faf7a78ab49d57a0bc22fc8de15da93433
Jan R <Jackjan@protonmail.com>
2024-08-30 13:43:37 +0200

    Record EventCallbacks now have return types

---
+++ BlazorExtensions/Components/Events/RecordEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Brandgroup.BlazorExtensions.Components.Bootstrap;
+
+namespace Brandgroup.BlazorExtensions.Components.Events;
+
+
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="record"></param>
+public class RecordEventArgs(object record) : EventArgs {
+
+
+
+    public object Record { get; set; } = record;
+}
---
+++ BlazorExtensions/Components/Events/RecordSelectEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Brandgroup.BlazorExtensions.Components.Events;
+
+
+
+public class RecordSelectEventArgs(object record, bool selected) : RecordEventArgs(record) {
+
+
+
+    public bool Selected { get; set; } = selected;
+}
