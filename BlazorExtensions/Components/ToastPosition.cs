f0844cfe1bfbabf9cdf57f72085a0955e8f45b6a
Jan R <Jackjan@protonmail.com>
2024-09-10 17:12:24 +0200

    Working on Toasts

---
+++ BlazorExtensions/Components/ToastPosition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brandgroup.BlazorExtensions.Components {
+    public enum ToastPosition {
+        TopLeft,
+        TopCenter,
+        TopRight,
+        MiddleLeft,
+        MiddleCenter,
+        MiddleRight,
+        BottomLeft,
+        BottomCenter,
+        BottomRight,
+    }
+}
